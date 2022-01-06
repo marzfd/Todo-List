ff9851d8bae0d58863386be42349cbe5ff50c9a2
Marzieh <mar.31069@gmail.com>
2022-01-06 13:06:09 +0330

    Change routes

--- server.js
+++ server.js
@@ -7,50 +7,50 @@ import { getUser, createUser, updateUser, deleteUser, checkUser } from "./src/us
 const app = express();
 app.use(express.json());
 
-app.use('/', express.static(path.join(path.resolve(), 'public')));
+app.use('/', express.static(path.join(path.resolve(), 'client/build')));
 
 // ... CATEGORIES ... //
 // Get Category
-app.get('/categories', (req, res) => getCategory(req, res));
+app.get('/api/categories', (req, res) => getCategory(req, res));
 
 // Create Category
-app.post('/categories', (req, res) => createCategory(req, res));
+app.post('/api/categories', (req, res) => createCategory(req, res));
 
 // Update Category
-app.put('/categories/:id', (req, res) => updateCategory(req, res));
+app.put('/api/categories/:id', (req, res) => updateCategory(req, res));
 
 // Delete Category
-app.delete('/categories/:id', (req, res) => deleteCategory(req, res));
+app.delete('/api/categories/:id', (req, res) => deleteCategory(req, res));
 
 // ... TASKS ... //
 // Get Task
-app.get('/tasks', (req, res) => getTask(req, res));
+app.get('/api/tasks', (req, res) => getTask(req, res));
 
 // Create Task
-app.post('/tasks', (req, res) => createTask(req, res));
+app.post('/api/tasks', (req, res) => createTask(req, res));
 
 // Update Task
-app.put('/tasks/:id', (req, res) => updateTask(req, res));
+app.put('/api/tasks/:id', (req, res) => updateTask(req, res));
 
 // Delete Task
-app.delete('/tasks/:id', (req, res) => deleteTask(req, res))
+app.delete('/api/tasks/:id', (req, res) => deleteTask(req, res))
 
 // ... USERS ... //
 // Get User
-app.get('/users', (req, res) => getUser(req, res));
+app.get('/api/users', (req, res) => getUser(req, res));
 
 // Create User
-app.post('/users', (req, res) => createUser(req, res));
+app.post('/api/users', (req, res) => createUser(req, res));
 
 // Update User
-app.put('/users/:username', (req, res) => updateUser(req, res));
+app.put('/api/users/:username', (req, res) => updateUser(req, res));
 
 // Delete User
-app.delete('/users/:username', (req, res) => deleteUser(req, res));
+app.delete('/api/users/:username', (req, res) => deleteUser(req, res));
 
 // Check if user exists
-app.get('/p/:tagId', (req, res) => checkUser(req, res));
+app.get('/api/p/:tagId', (req, res) => checkUser(req, res));
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
