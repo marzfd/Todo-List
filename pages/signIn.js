2c9e4c3b07e69ed0660ffbbe5af4152b6b31608b
Marzieh <mar.31069@gmail.com>
2022-02-21 22:29:55 +0330

    Add function

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,7 +1,44 @@
+import { useState } from 'react'
 import Link from "next/link"
 import Image from "next/image"
+import Router from 'next/router'
 
 const signIn = () => {
+
+  const [username, setUsername] = useState("")
+  // const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(username)
+    if (!username) {
+      setError("Username is required !")
+    } else {
+      setError("")
+      fetch(`/api/users/${username}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.error) {
+          setError('Username does not exist !')
+        } else {
+          console.log(data)
+          setTimeout(() => {
+            Router.push({
+              pathname: '/profile',
+              query: {
+                name: data.name,
+                username: data.username,
+                email: data.email
+              }
+            })
+          }, 1000);
+        }
+      })
+      .catch(err => console.log(err))
+    }
+  }
+
   return (
     <div>
       <main className='mx-auto lg:w-9/12 p-5 md:p-10 flex flex-wrap'>
@@ -22,25 +59,56 @@ const signIn = () => {
         </div>
         <div className='mx-auto bg-white p-4 md:p-8 rounded-lg shadow-2xl h-min md:w-1/2 lg:w-1/3 w-full self-center'>
           <h2 className='mb-5 font-semibold'>Sign in to your account</h2>
-          <form className='space-y-4'>
+          <form className='space-y-8'>
             <div>
-              <input type='text' placeholder="Username" className='text-sm border-b-2 border-purple-200 placeholder-purple-300 px-3 py-2 focus:outline-none focus:border-purple-400 w-full'/>
+              <input
+                type='text'
+                placeholder="Username"
+                onChange={e => setUsername(e.target.value)}
+                className='text-sm border-b-2 border-purple-200 placeholder-purple-300 px-3 py-2 focus:outline-none focus:border-purple-400 w-full'
+              />
             </div>
             <div>
-              <input type='password' disabled placeholder="Password" className='text-sm border-b-2 border-purple-200 placeholder-purple-300 px-3 py-2 focus:outline-none focus:border-purple-400 w-full'/>
+              <input
+                type='password'
+                placeholder="Password"
+                className='text-sm border-b-2 border-purple-200 placeholder-purple-300 px-3 py-2 focus:outline-none focus:border-purple-400 w-full'
+                disabled
+              />
             </div>
-            <div className="flex items-center text-gray-500">
-              <input type='checkbox' className='mr-2 mt-3 mb-3'/>
+            {/* <div className="flex items-center text-gray-500">
+              <input
+                type='checkbox'
+                className='mr-2 mt-3 mb-3'
+              />
               <span className='text-xs'>Remember me</span>
-            </div>
+            </div> */}
             <div>
-              <Link href='/profile'>
-                <a>
-                  <button type="submit" className='bg-purple-700 hover:bg-purple-500 transition duration-300 text-white w-full py-2 px-4 rounded-lg focus:outline-none'>Sign In</button>
-                </a>
-              </Link>
+              <button
+                type="submit"
+                onClick={onSubmit}
+                className='bg-purple-700 hover:bg-purple-500 transition duration-300 text-white w-full py-2 px-4 rounded-lg focus:outline-none'
+                >
+                  Sign In
+              </button>
             </div>
           </form>
+          {
+            error
+            &&
+            <div className="relative py-3 my-5 pl-4 pr-10 leading-normal text-red-700 bg-red-100 rounded-lg" role="alert">
+              <p>{error}</p>
+              <span className="absolute inset-y-0 right-0 flex items-center mr-4">
+                <button
+                  type="button"
+                  onClick={() => setError(null)}
+                  className="text-red-700 focus:outline-none focus:shadow-outline"
+                >
+                  <svg className="w-4 h-4 fill-current" role="button" viewBox="0 0 20 20"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
+                </button>
+              </span>
+            </div>
+          }
         </div>
         <div className='mt-20 md:hidden'>
             <p className='text-lg mt-2 mb-3'>Don&apos;t have an account?</p>
