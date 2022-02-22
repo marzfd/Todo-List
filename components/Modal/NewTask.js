d5bd97d2bef0597df4b29f2d761b567b9e5f25c1
Marzieh <mar.31069@gmail.com>
2022-02-22 12:18:37 +0330

    Add functions

--- components/Modal/NewTask.js
+++ components/Modal/NewTask.js
@@ -1,9 +1,42 @@
 import { useState } from 'react';
+import Link from 'next/link';
 
-const NewTask = () => {
+const NewTask = ({ categories }) => {
   const [modal, setModal] = useState(false);
+  const [task, setTask] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [error, setError] = useState('');
 
   const toggle = () => setModal(!modal);
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(task, categoryId);
+    if (!task && !categoryId) {
+      setError('Please enter all fields !')
+    } else {
+
+      console.log(categories);
+      console.log(categoryId);
+      console.log(task);
+
+      fetch('/api/tasks', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          task_name: task,
+          category_id: parseInt(categoryId),
+          username: 'marzi',
+        })
+      })
+      .then(res => res.json())
+      .then(data => {
+        alert(data.message)
+        window.location.reload()
+      })
+      .catch(err => console.log(err))
+    }
+  }
 
   return (
     <>
@@ -21,16 +54,63 @@ const NewTask = () => {
                 </button>
               </div>
               <div className='mt-6'>
-                <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-purple-200 placeholder-purple-300' type='text' placeholder='Task Name' required />
+                <input
+                  type='text'
+                  value={task}
+                  onChange={e => setTask(e.target.value)}
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-purple-200 placeholder-purple-300' type='text' placeholder='Task Name' required
+                />
               </div>
               <div className='mt-6'>
-                <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-purple-200 placeholder-purple-300' type='text' placeholder='Category Name' required />
+                <select
+                  value={categoryId}
+                  onChange={e => setCategoryId(e.target.value)}
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-purple-500 leading-tight focus:outline-none focus:shadow-outline border-purple-200 placeholder-purple-300' type='text' placeholder='Category Name' required>
+                  <option value='' className='text-purple-300'>Choose a Category</option>
+                    {categories &&
+                      categories.map(category => (
+                        <option key={category.category_id} value={category.category_id}>{category.category_name}</option>
+                    ))}
+                </select>
               </div>
               <div className='mt-6'>
-                <button className='bg-purple-700 hover:bg-purple-500 transition duration-300 text-white w-full py-2 px-4 rounded-lg focus:outline-none'>
+                <button
+                  type='submit'
+                  onClick={onSubmit}
+                  className='bg-purple-700 hover:bg-purple-500 transition duration-300 text-white w-full py-2 px-4 rounded-lg focus:outline-none'
+                >
                   Add Task
                 </button>
               </div>
+              <Link href='/categories'>
+                <a>
+                  <div className='mt-6'>
+                    <button
+                      type='submit'
+                      onClick={toggle}
+                      className='bg-orange-500 hover:bg-orange-400 transition duration-300 text-white w-full py-2 px-4 rounded-lg focus:outline-none'
+                    >
+                      Add New Category
+                    </button>
+                  </div>
+                </a>
+              </Link>
+              {
+                error
+                &&
+                <div className="relative py-3 my-5 pl-4 pr-10 leading-normal text-red-700 bg-red-100 rounded-lg" role="alert">
+                  <p>{error}</p>
+                  <span className="absolute inset-y-0 right-0 flex items-center mr-4">
+                    <button
+                      type="button"
+                      onClick={() => setError(null)}
+                      className="text-red-700 focus:outline-none focus:shadow-outline"
+                    >
+                      <svg className="w-4 h-4 fill-current" role="button" viewBox="0 0 20 20"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
+                    </button>
+                  </span>
+                </div>
+              }
             </div>
           </div>
         </div>
