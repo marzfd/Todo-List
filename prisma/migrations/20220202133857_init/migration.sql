ef68d04c3a02bab0df6cb5b43ce33bcfc4f4fdb3
Marzieh <mar.31069@gmail.com>
2022-02-24 14:35:04 +0330

    Change Prisma

--- prisma/migrations/20220202133857_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `registerAt` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `registerAt`,
-    ADD COLUMN `register_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
--- prisma/migrations/20220202142313_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `task` MODIFY `is_done` BOOLEAN NOT NULL DEFAULT false;
